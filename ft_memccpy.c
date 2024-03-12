3f1bc0fd086a560433aac80d954f541a0dd48d53
sfazzello01 <sfazzelo01@gmail.com>
2024-03-12 17:00:30 +0100

    mia

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,27 +6,36 @@
 /*   By: sfazzell <sfazzell@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 12:23:58 by sfazzell          #+#    #+#             */
-/*   Updated: 2024/03/12 15:32:42 by sfazzell         ###   ########.fr       */
+/*   Updated: 2024/03/12 16:59:47 by sfazzell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
-	unsigned char	*dst_cpy;
-	unsigned char	*src_cpy;
-	size_t			i;
-
-	dst_cpy = (unsigned char *)dst;
-	src_cpy = (unsigned char *)src;
+	size_t	i;
+	
 	i = 0;
 	while (i < n)
 	{
-		dst_cpy[i] = src_cpy[i];
-		if (src_cpy[i] == (unsigned char)c)
-			return (dst_cpy + i + 1);
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+		if (((unsigned char *)src)[i] == (unsigned char)c)
+			return (dst + i + 1);
 		i++;
 	}
 	return (NULL);
+}
+
+int main (void)
+{
+	char src[] = "Hello, World!";
+	char dest[40];
+	char *ret;
+
+	ret = ft_memccpy(dest, src, 'e', 12);
+	printf("After memccpy dest = %s\n", dest);
+	printf("Returned string = %s\n", ret);
+	return (0);
 }
